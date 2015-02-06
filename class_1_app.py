53fe7309ff20cf81a3ceb9034e998d6c5aefa1b4
sophiajose <joses533@newschool.edu>
2015-02-06 10:46:30 -0500

    commit1

--- class_1_app.py
+++ class_1_app.py
@@ -4,12 +4,19 @@ import math
 a = 10
 c = 8
 
+
+rs.EnableRedraw(False)
 for u in range (0,100):
     for v in range (0,100):
         x = (c+a*math.cos (v))*math.cos(u)
         y = (c+a*math.cos(v))*math.sin (u)
         z = a*math.sin (v)
         pt = rs.AddPoint(x,y,z)
+        print(z)
+        sphere = rs.AddSphere([x,y,z],z)
+        
+
+rs.EnableRedraw(True)
 
 
 
