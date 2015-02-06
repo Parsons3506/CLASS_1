c6cb99bf284524346c302b69e8af28099c9ea2d9
wrigj178 <wrigj178@newschool.edu>
2015-02-06 10:47:25 -0500

    2.6.15 BreakTime
    where we left off

--- class_1_app.py
+++ class_1_app.py
@@ -1,18 +1,18 @@
 import rhinoscriptsyntax as rs
 import math
 
-a=2
-c=5
+a=1
+c=15
 
 rs.EnableRedraw(False)
-for v in range (0,100):
-    for u in range (0,100):
+for v in range (0,50):
+    for u in range (0,50):
        x = (c+a*math.cos(v))*math.cos(u)
        y = (c+a*math.cos(v))*math.sin(u)
        z = a*math.sin(v)
        pt = rs.AddPoint( x,y,z)
        print(z)
-       sphere = rs.AddSphere (pt,z)
+       sphere = rs.AddSphere ([x,y,z],z)
 
 rs.EnableRedraw(True)
 
