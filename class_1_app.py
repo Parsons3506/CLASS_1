9a8d27b8214aca998e637e7a89ea19543a82b693
Bradley Rothenberg <Brad@bradleyrothenberg.com>
2015-02-06 10:46:45 -0500

    torus generator

--- class_1_app.py
+++ class_1_app.py
@@ -1,8 +1,18 @@
 import rhinoscriptsyntax as rs
+import math
 
+a = 1
+c = 10
 
-x=0
-y = 0
-z = 0
+rs.EnableRedraw(False)
 
-pt = rs.AddPoint(x,y,z)
+for v in range(0,50):
+    for u in range(0,50):
+        x = (c+a*math.cos(v))*math.cos(u)
+        y = (c+a*math.cos(v))*math.sin(u)
+        z = a*math.sin(v)
+        pt = rs.AddPoint(x,y,z)
+        print(z)
+        sphere = rs.AddSphere([x,y,z],z)
+
+rs.EnableRedraw(True)
