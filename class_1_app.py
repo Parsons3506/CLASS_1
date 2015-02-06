5bbdc174cfe4be2a4779796a320e2ec9b0b6f62b
DaniABorges <aguid286@newschool.edu>
2015-02-06 10:46:03 -0500

    Loops

--- class_1_app.py
+++ class_1_app.py
@@ -1,10 +1,21 @@
 import rhinoscriptsyntax as rs
+import math
 
-x = 0
-y = 0
-z = 0
+a = 1
+c = 10
 
-point = rs.AddPoint(x,y,z)
+rs.EnableRedraw(False)
+
+for u in range(0,50):
+    for v in range(0,50):
+        x = (c+a*math.cos(v))*math.cos(u)
+        y = (c+a*math.cos(v))*math.sin(u)
+        z = a*math.sin(v)
+        pt = rs.AddPoint(x,y,z)
+        print(z)
+        sphere = rs.AddSphere([x,y,z],z)
+        
+        rs.EnableRedraw(True)
 
 
 
