2ab26976c6bdbe645801db9b732fd0eb0e87b383
sophiajose <joses533@newschool.edu>
2015-02-06 10:28:45 -0500

    commit2

--- class_1_app.py
+++ class_1_app.py
@@ -1,1 +1,15 @@
-print "HELLO SOPHIA"
+import rhinoscriptsyntax as rs
+import math
+
+a = 10
+c = 8
+
+for u in range (0,100):
+    for v in range (0,100):
+        x = (c+a*math.cos (v))*math.cos(u)
+        y = (c+a*math.cos(v))*math.sin (u)
+        z = a*math.sin (v)
+        pt = rs.AddPoint(x,y,z)
+
+
+
