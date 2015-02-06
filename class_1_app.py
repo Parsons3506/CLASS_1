85214eedca0594f7f43438d54f3b0ab2732fe42a
consk007 <consk007@newschool.edu>
2015-02-06 10:26:08 -0500

    commit_makingtorus

--- class_1_app.py
+++ class_1_app.py
@@ -1,7 +1,15 @@
 import rhinoscriptsyntax as rs
+import math
 
-x = 0
-y = 0
-z = 0
+
+a = 1
+c = 10
+
+for u in range(50,100):
+    for v in range(0,50):
+        x = (c+a*math.cos(v))*math.cos(u)
+        y = (c+a*math.cos(v))*math.sin(u)
+        z = a*math.sin(v)
+        pt = rs.AddPoint(x,y,z)
 
 rs.AddPoint(x,y,z)
