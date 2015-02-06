f8434eb77829ead9fe1ce9ffc8f22acd99a84c10
Anh Le <lea100@newschool.edu>
2015-02-06 10:20:40 -0500

    Torus

--- class_1_app.py
+++ class_1_app.py
@@ -1,1 +1,17 @@
-print "ANH"
+import rhinoscriptsyntax as rs
+import math
+
+a = 1
+c = 10
+
+for u in range(0,100):
+    for v in range(0,100):
+        x=(c+a*math.cos(v))*math.cos(u)
+        y=(c+a*math.cos(v))*math.sin(u)
+        z=a*math.sin(v)
+        rs.AddPoint(x,y,z)
+        pt = rs.AddPoint
+
+
+
+
