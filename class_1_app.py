1683829f7d262eee42d18358a322bcf2ac579c20
bbaerwolf <baerb638@newschool.edu>
2015-02-06 10:28:06 -0500

    Functions

--- class_1_app.py
+++ class_1_app.py
@@ -1,1 +1,16 @@
-print "BAILEY BAERWOLF"
+import rhinoscriptsyntax as rs
+import math
+a = 10
+c = 100
+
+rs.EnableRedraw(False)
+
+for u in range(0,100):
+    for v in range (0,100):
+        x =(c+a*math.cos(v))*math.cos(u)
+        y = (c+a*math.cos(v))*math.sin(u)
+        z = a*math.sin(v)
+        pt = rs.AddPoint(x,y,z)
+        sphere = rs.AddSphere(pt, z)
+        
+rs.EnableRedraw(True)
