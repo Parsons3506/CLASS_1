7908d6b410c4f4b9b2a783057dea8997e3209aa8
BriaBlunt <blunt635@newschool.edu>
2015-02-06 10:25:20 -0500

    Bria Commit 2

--- class_1_app.py
+++ class_1_app.py
@@ -1,1 +1,15 @@
-print "Bria Blunt"
+
+import rhinoscriptsyntax as rs
+import math
+
+a = 11
+c = 5
+
+for u in range(0,100):
+    for v in range (0,100):
+        x = (c+a*math.cos(v))*math.cos(u)
+        y = (c+a*math.cos(v))*math.sin(u)
+        z = a*math.sin(v)
+        pt = rs.AddPoint(x,y,z)
+
+pt = rs.AddPoint(x,y,z)
