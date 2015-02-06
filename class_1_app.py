cbc2440f26d2ef6d1d118a4244d42eb612486980
wrigj178 <wrigj178@newschool.edu>
2015-02-06 11:35:48 -0500

    Branching Lesson

--- class_1_app.py
+++ class_1_app.py
@@ -1,19 +1,41 @@
 import rhinoscriptsyntax as rs
 import math
 
-a=1
-c=15
 
-rs.EnableRedraw(False)
-for v in range (0,50):
-    for u in range (0,50):
-       x = (c+a*math.cos(v))*math.cos(u)
-       y = (c+a*math.cos(v))*math.sin(u)
-       z = a*math.sin(v)
-       pt = rs.AddPoint( x,y,z)
-       print(z)
-       sphere = rs.AddSphere ([x,y,z],z)
+def main():
+    pt = rs.AddPoint(0,0,0)
+    vecDir = [0,0,1]
+    
+    
+    min
+    
+    
+    Props = minTwigCount, minTwigCount, maxGen, maxTwigLength, lengthMutation, maxTwigAngle, angleLengthMutation = props
 
-rs.EnableRedraw(True)
+def Add
 
+def randomPointInCone (orgin, direction, minDistance, maxDistance, maxAngle):
+    vecTwig = rs.VectorUnitise(direction)
+    vecTwig = rs.VectorScale(vecTwig,minDistance + random.random()*
 
+def RecursiveGrowth(ptStart, vecDir, props, gen):
+    minTwigCount, minTwigCount, maxGen, maxTwigLength, lengthMutation, maxTwigAngle, angleLengthMutation = props
+    if gen > maxGen : return
+    newProps = props
+    maxTwiglength *= lengthMutation 
+    maxTwigAngle *= angleMutation 
+    
+    if maxTwigAngle>90:maxTwigAngle=90
+    
+    minTwigCount, minTwigCount, maxGen, maxTwigLength, lengthMutation, maxTwigAngle, angleLengthMutation = props
+    
+    maxN=int(minTwigCount + random() *(maxTwigCount-minTwigCount) )
+    for n in range(0, maxN):
+    
+    newPoint = RandomPointInCone (pointStart, vecDir, p25*maxTwigLength, maxTwigAngle
+    newTwig = AddArcDir(ptStart, newPoint, vecDir)
+    if newTwig:
+        vecGrow = rs.CurveTangent(newTwig, rs.CurveDomain (newTwig) (1))
+        RecursiveGrowth(newPoint, vecGrow,
+
+if__name__=="main
