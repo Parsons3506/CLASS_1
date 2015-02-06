d00c46fa613920f44dc3fed012e5c135f7fa65d2
wrigj178 <wrigj178@newschool.edu>
2015-02-06 10:30:30 -0500

    Sphere
    Runs faster and views the z value

--- class_1_app.py
+++ class_1_app.py
@@ -4,14 +4,16 @@ import math
 a=2
 c=5
 
-
+rs.EnableRedraw(False)
 for v in range (0,100):
     for u in range (0,100):
        x = (c+a*math.cos(v))*math.cos(u)
        y = (c+a*math.cos(v))*math.sin(u)
        z = a*math.sin(v)
        pt = rs.AddPoint( x,y,z)
+       print(z)
+       sphere = rs.AddSphere (pt,z)
 
-
+rs.EnableRedraw(True)
 
 
