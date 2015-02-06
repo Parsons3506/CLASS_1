2c451ab9ea60edc7ff6003c773763f6c4afce94c
wrigj178 <wrigj178@newschool.edu>
2015-02-06 10:26:28 -0500

    points tori

--- class_1_app.py
+++ class_1_app.py
@@ -1,1 +1,17 @@
-print "HELLO Jasmine"
+import rhinoscriptsyntax as rs
+import math
+
+a=2
+c=5
+
+
+for v in range (0,100):
+    for u in range (0,100):
+       x = (c+a*math.cos(v))*math.cos(u)
+       y = (c+a*math.cos(v))*math.sin(u)
+       z = a*math.sin(v)
+       pt = rs.AddPoint( x,y,z)
+
+
+
+
