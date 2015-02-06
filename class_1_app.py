57584ef7cd4bc473055a3f10eb77b6e5fadb052c
bbaerwolf <baerb638@newschool.edu>
2015-02-06 11:34:09 -0500

    Tree

--- class_1_app.py
+++ class_1_app.py
@@ -2,6 +2,26 @@ import rhinoscriptsyntax as rs
 import math
 import random
 
+def main ():
+    pt = rs.AddPoint(0,0,0)
+    vecDir [0,0,1]
+    
+    minTwigCount = 1
+    maxTwigCount = 2
+    maxGen = 3
+    maxTwigLength = 10
+    lengthMutation = .5
+    maxTwigAngle = 90
+    angleMutation = .5
+    
+    newProps = minTwigCount, maxTwigCount, maxGen, maxTwigLength, lengthMutation, maxTwigAngle, angleMutation
+    
+    RecursiveGrowth (ptStart, vecDir, props, 0)
+    
+def AddArcDir (ptStart, ptEnd, vecDir):
+    
+    
+
 def RandomPointInCone(origin, direction, minDistance, maxDistance, maxAngle):
     vecTwig = rs.VectorUnitize (direction)
     vecTwig = rs.VectorScale (vecTwig, minDistance + random.random()* (maxDistance-minDistance))
@@ -30,3 +50,7 @@ def RecursiveGrowth (ptStart, vedDi, props, gen):
             vecGrow = rs.CurveTangent(newTwig, rs.CurveDomain(newTwig)[1])
             RecursiveGrowth(newPoint, vecGrow, newProps, gen+1)
             
+            
+if __name__ == "__main__"
+    main()
+    
