01fed3cd88e4cd1fe4a93d60926310f94e8b600c
L Gallinaro <lg@glnro.com>
2015-02-13 10:11:23 -0500

    Python Commit 2

--- Branching_1.py
+++ Branching_1.py
@@ -6,21 +6,23 @@ import random
 
 def main():
     
+    aPoint = rs.GetPoint("Pick Point")
+    
     ptStart = rs.AddPoint(0,0,0)
     vecDir = [0,0,1]
     
     minTwigCount = 1 
     maxTwigCount = 5
-    maxGen = 5
+    maxGen = 10
     maxTwigLength = 1
     lengthMutation = .5
-    maxTwigAngle = 90
+    maxTwigAngle = 270
     angleMutation = .5
     
     
     props = minTwigCount, maxTwigCount, maxGen, maxTwigLength, lengthMutation,maxTwigAngle, angleMutation
     
-    RecursiveGrowth(ptStart, vecDir, props, 0)
+    RecursiveGrowth(ptStart, vecDir, props, 0, aPoint)
 
 
 def AddArcDir(ptStart, ptEnd, vecDir):
@@ -34,7 +36,8 @@ def AddArcDir(ptStart, ptEnd, vecDir):
     dotProd = rs.VectorDotProduct(vecBisector, vecDir)
     midLength = (0.5*rs.Distance(ptStart, ptEnd))/dotProd
     vecBisector = rs.VectorScale(vecBisector, midLength)
-    return rs.AddArc3Pt(ptStart, rs.PointAdd(ptStart, vecBisector), ptEnd)
+    #return rs.AddArc3Pt(ptStart, rs.PointAdd(ptStart, vecBisector), ptEnd)
+    return rs.AddLine(ptStart, ptEnd)
 
 
 
@@ -48,7 +51,7 @@ def RandomPointInCone(origin, direction, minDistance, maxDistance, maxAngle):
 
 
 
-def RecursiveGrowth(ptStart, vecDir, props, gen):
+def RecursiveGrowth(ptStart, vecDir, props, gen, aPoint):
     minTwigCount, maxTwigCount, maxGen, maxTwigLength, lengthMutation,maxTwigAngle, angleMutation = props
     
     if gen > maxGen : return
@@ -65,11 +68,13 @@ def RecursiveGrowth(ptStart, vecDir, props, gen):
     maxN=int(minTwigCount+random.random()* (maxTwigCount-minTwigCount) )
     
     for n in range(0,maxN):
+        newVector = rs.VectorCreate(aPoint, ptStart)
+        vecDir = newVector
         newPoint = RandomPointInCone(ptStart, vecDir, .25*maxTwigLength, maxTwigLength, maxTwigAngle)
         newTwig = AddArcDir(ptStart, newPoint, vecDir)
         if newTwig:
             vecGrow = rs.CurveTangent(newTwig, rs.CurveDomain(newTwig)[1])
-            RecursiveGrowth(newPoint, vecGrow, newProps, gen+1)
+            RecursiveGrowth(newPoint, vecGrow, newProps, gen+1, aPoint)
             
             
             
