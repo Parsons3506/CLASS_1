f2af9ebfa8417651493a9b4c32522b8622c7db0b
lea100 <lea100@newschool.edu>
2015-02-13 09:33:37 -0500

    branching1

--- class_1_app.py
+++ class_1_app.py
@@ -1,25 +1,6 @@
 import rhinoscriptsyntax as rs
 import math
-<<<<<<< HEAD
-
-a = 1
-c = 10
-
-rs.EnableRedraw(False)
-
-for u in range(0,50):
-    for v in range(0,50):
-        x = (c+a*math.cos(v))*math.cos(u)
-        y = (c+a*math.cos(v))*math.sin(u)
-        z = a*math.sin(v)
-        pt = rs.AddPoint(x,y,z)
-        print z
-        sphere = rs.AddSphere([x,y,z],z)
-
-rs.EnableRedraw(True)
-=======
 import random
-
 
 
 def main():
@@ -95,7 +76,6 @@ def RecursiveGrowth(ptStart, vecDir, props, gen):
 if __name__ == "__main__":
     main()
 
->>>>>>> origin/NEW-MASTER
 
 
 
